454151e02cb740fad030ecfcc4858990acd2bd9c
Shaun Minnihan <spminnihan@gmail.com>
2019-10-18 18:48:22 -0500

    part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'A random assortment of images:';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.kym-cdn.com/entries/icons/original/000/030/054/moonhaunted.jpg';
+  image3 = 'https://i.kym-cdn.com/photos/images/original/001/584/767/cd7.jpg_large';
 
   constructor() { }
 
