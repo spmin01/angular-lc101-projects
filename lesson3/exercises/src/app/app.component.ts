0d6d7a289495f8c23f8c5db8aa889d552bd036cf
Shaun Minnihan <spminnihan@gmail.com>
2019-10-25 22:56:57 -0500

    lesson 3 exercise w/ bonus done

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,86 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = true;
+
+  handleTakeOff(rocketImage) {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      let movement = String(parseInt(rocketImage.style.bottom) + 10) + 'px';
+      rocketImage.style.bottom = movement;
+      this.height += 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+      this.takeOffEnabled = false;
+    }
+  }
+
+  handleLand(rocketImage) {
+    window.alert("The shuttle is landing. Landing gear engaged.");
+    this.message = "The shuttle has landed";
+    this.color = 'green';
+    this.height = 0;
+    rocketImage.style.bottom = '0px'
+    this.takeOffEnabled = true;
+  }
+
+  handleAbort(rocketImage) {
+    let result = window.confirm("Abort mission?");
+    if (result) {
+      this.message = "Mission aborted!";
+      this.color = 'green';
+      this.height = 0;
+      rocketImage.style.bottom = '0px'
+      this.takeOffEnabled = true;
+    }
+  }
+
+  moveRocket(rocketImage, direction: string) {
+    if (direction === 'right' && !(this.width >= 460000)) {
+      let movement = String(parseInt(rocketImage.style.left) + 10) + 'px';
+      rocketImage.style.left = movement;
+      this.width += 10000;
+      if (this.edgeWarning()) {
+        this.color = 'orange';
+      } else {
+        this.color = 'blue';
+      }
+    } else if (direction === 'left' && !(this.width <= 0)) {
+      let movement = String(parseInt(rocketImage.style.left) - 10) + 'px';
+      rocketImage.style.left = movement;
+      this.width -= 10000;
+      if (this.edgeWarning()) {
+        this.color = 'orange';
+      } else {
+        this.color = 'blue';
+      }
+    } else if (direction === 'up' && !(this.height >= 320000)) {
+      let movement = String(parseInt(rocketImage.style.bottom) + 10) + 'px';
+      rocketImage.style.bottom = movement;
+      this.height += 10000;
+      if (this.edgeWarning()) {
+        this.color = 'orange';
+      } else {
+        this.color = 'blue';
+      }
+    } else if (direction === 'down' && !(this.height <= 0)) {
+      let movement = String(parseInt(rocketImage.style.bottom) - 10) + 'px';
+      rocketImage.style.bottom = movement;
+      this.height -= 10000;
+      if (this.edgeWarning()) {
+        this.color = 'orange';
+      } else {
+        this.color = 'blue';
+      }
+    }
+  }
+
+  edgeWarning(): boolean {
+    if (this.width <= 0 || this.width >= 460000 || this.height <= 0 || this.height >= 320000) {
+      return true;
+    }
+    return false;
+  }
+
 }
